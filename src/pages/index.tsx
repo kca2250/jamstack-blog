d8d2ea340bf1cacc39d60a0b31f1b46252aee84b
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2023-10-19 22:05:18 +0900

    Feature/introduced micro cms (#2)
    * install microcms-js-sdk
    
    * microCMSのclientを作成
    
    * Blogの型を作成
    
    * 記事一覧ページ作成
    
    * 記事詳細ページを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <p>this is jamstack blog</p>;
+import Link from "next/link";
+import { client } from "../libs/client";
+import { Blog } from "../types/blog";
+
+export default function Home({ blog }: { blog: Blog[] }) {
+  return (
+    <div>
+      <ul>
+        {blog.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
---
+++ src/types/blog.ts
@@ -0,0 +1,19 @@
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  category: Category;
+};
+
+export type Category = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
+};
