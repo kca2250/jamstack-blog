265cc8d360a4a55348bfb1527df219545817b755
kca2250 <2250katok@gmail.com>
2023-11-08 20:25:54 +0900

    軽微な修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Layout } from "./_layout";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
   )
 }
