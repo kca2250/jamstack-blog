62fc58b45113081e7067778ecf2bcc91614e0ec6
kca2250 <2250katok@gmail.com>
2023-11-08 20:25:46 +0900

    custom fontを導入

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,7 +1,19 @@
-export const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className='m-auto px-4 py-2 max-w-screen-md'>
-      {children}
-    </div>
-  )
-}
+import { Header } from "@/components/header"
+import { Noto_Sans_JP, Ubuntu } from 'next/font/google';
+
+const notoSansJp = Noto_Sans_JP({
+  subsets: ['latin'],
+  weight: ['500', '800']
+})
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
+
+
+export const Layout = ({ children }: { children: React.ReactNode }) => (
+  <div className={`${ubuntu.className} ${notoSansJp.className} m-auto px-4 py-10 max-w-screen-md flex flex-col gap-10`}>
+    <Header />
+    {children}
+  </div>
+)
