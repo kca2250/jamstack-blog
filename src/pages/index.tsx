363b3132998c3a981d53c649538ab95a3964bf66
kca2250 <2250katok@gmial.com>
2023-11-09 23:07:49 +0900

    レイアウトの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
 import Link from "next/link";
 import { client } from "../libs/client";
 import { Blog } from "../types/blog";
+import dayjs from "dayjs";
 
 export default function Home({ blog }: { blog: Blog[] }) {
+
   return (
     <div>
       <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
+        {blog.map((blog) => {
+          const publishedAt = dayjs(blog.publishedAt).format("YYYY/MM/DD");
+          return (
+
+            <li key={blog.id} className="py-4 px-2 rounded hover:bg-slate-100 hover:transition-all hover:duration-300">
+              <Link href={`/blog/${blog.id}`} className="text-lg text-slate-800 flex flex-wrap">
+                <p className="underline mr-5">{publishedAt}</p>
+                <p>{blog.title}</p>
+              </Link>
+            </li>
+          )
+        }
+        )}
       </ul>
     </div>
   );
