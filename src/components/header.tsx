e7561695dbe2b32f54a3193077c56161df54c04f
kca2250 <2250katok@gmial.com>
2023-11-09 23:07:34 +0900

    タイトル、ロゴ、スタイルの修正

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
-import { BsInboxes } from 'react-icons/bs';
+import Link from 'next/link';
+import { TbNotebook } from 'react-icons/tb';
 
 export const Header = () => (
   <header className='flex flex-wrap items-center justify-between gap-2'>
-    <div className='flex items-center gap-2'>
-      <BsInboxes size='1.8em' />
-      <h1 className='text-xl font-bold'>knowledge box</h1>
-    </div>
+    <Link href='/' className='flex items-center'>
+      <TbNotebook size='1.8em' />
+      <h1 className='text-xl font-bold underline'>knowledge note.</h1>
+    </Link>
     <p className='text-sm text-gray-500'>某フロントエンドエンジニアの備忘録</p>
   </header>
 )
