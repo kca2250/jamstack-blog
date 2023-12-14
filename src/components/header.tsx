31a4fc26ad6922a687befe22c3cee62591ce3211
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2023-12-15 01:34:00 +0900

    Feature/create meta (#10)
    * サイト名を変更
    
    * descriptionを追加
    
    * meta情報を追加

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => (
   <header className='flex flex-wrap items-center justify-between gap-2'>
     <Link href='/' className='flex items-center gap-1' >
       <Image src={'/logo.svg'} alt='logo-image' width={30} height={30} />
-      <h1 className='text-xl font-bold underline'>knowledge note.</h1>
+      <h1 className='text-xl font-bold underline'>kca2250 log</h1>
     </Link>
     <p className='text-sm text-gray-500'>某フロントエンドエンジニアの備忘録</p>
   </header>
