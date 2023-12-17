601fcda4b96d6a45801068abba2d21d5bef8fd00
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2023-12-17 23:54:28 +0900

    Feature/modify meta data (#13)
    * トップページにmetaデータを配置
    
    * aboutページにmetaデータを配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { client } from "../libs/client";
 import { Blog } from "../types/blog";
 import dayjs from "dayjs";
+import HeadMeta from "@/components/headMeta";
 
 export default function Home({ blog }: { blog: Blog[] }) {
 
@@ -14,23 +15,32 @@ export default function Home({ blog }: { blog: Blog[] }) {
   }
 
   return (
-    <div>
-      <ul>
-        {blog.map((blog) => {
-          const publishedAt = dayjs(blog.publishedAt).format("YYYY/MM/DD");
-          return (
+    <>
+      <HeadMeta
+        title="記事一覧"
+        description='某フロントエンドエンジニアの備忘録'
+        type='website'
+        url=""
+        imageUrl=""
+      />
+      <div>
+        <ul>
+          {blog.map((blog) => {
+            const publishedAt = dayjs(blog.publishedAt).format("YYYY/MM/DD");
+            return (
 
-            <li key={blog.id} className="py-4 px-2 rounded hover:bg-slate-100 hover:transition-all hover:duration-300">
-              <Link href={`/blog/${blog.id}`} className="text-lg text-slate-800 flex flex-wrap">
-                <p className="underline mr-5">{publishedAt}</p>
-                <p>{blog.title}</p>
-              </Link>
-            </li>
-          )
-        }
-        )}
-      </ul>
-    </div>
+              <li key={blog.id} className="py-4 px-2 rounded hover:bg-slate-100 hover:transition-all hover:duration-300">
+                <Link href={`/blog/${blog.id}`} className="text-lg text-slate-800 flex flex-wrap">
+                  <p className="underline mr-5">{publishedAt}</p>
+                  <p>{blog.title}</p>
+                </Link>
+              </li>
+            )
+          }
+          )}
+        </ul>
+      </div>
+    </>
   );
 }
 
