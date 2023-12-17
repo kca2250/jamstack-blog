9d3802b6c2ad7ab6b59d6f74d3268747611a0740
kca2250 <2250katok@gmail.com>
2023-12-17 23:53:44 +0900

    aboutページにmetaデータを配置

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,15 @@
+import HeadMeta from "@/components/headMeta";
+
 export default function About() {
   return (
     <>
+      <HeadMeta
+        title="About Me"
+        description="kca2250の自己紹介です"
+        type="website"
+        url=""
+        imageUrl=""
+      />
       <p>this is about page.</p>
     </>
   )
