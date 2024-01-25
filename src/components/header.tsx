0c42ea8575becaa72a2f1d0c22a69d68196317da
kca2250 <2250katok@gmail.com>
2024-01-26 05:55:03 +0900

    ブログタイトル変更に伴う修正など

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,24 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
-export const Header = () => (
-  <header className='flex flex-wrap items-center justify-between gap-2'>
-    <Link href='/' className='flex items-center gap-1' >
-      <Image src={'/logo.svg'} alt='logo-image' width={30} height={30} />
-      <h1 className='text-xl font-bold underline'>kca2250 log</h1>
+export function Header() {
+  const currentRoute = useRouter();
+  const isAboutPage = currentRoute.asPath === "/about";
+
+  const displayAboutPage = isAboutPage ? null : (
+    <Link href="/about" className="text-gray-700 underline">
+      about me
     </Link>
-    <p className='text-sm text-gray-500'>某フロントエンドエンジニアの備忘録</p>
-  </header>
-)
+  );
+
+  return (
+    <header className="flex flex-wrap items-center justify-between">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src={"/logo.svg"} alt="logo-image" width={30} height={30} />
+        <h1 className="text-xl font-bold">biii_grrr</h1>
+      </Link>
+      {displayAboutPage}
+    </header>
+  );
+}
