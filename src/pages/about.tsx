c07fe241135a4ef7c8b3455541da71a89f9edf23
kca2250 <2250katok@gmail.com>
2024-01-26 23:04:35 +0900

    軽微な修正

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,17 +1,16 @@
-import HeadMeta from "@/components/headMeta";
+import { HeadMeta } from "@/components/headMeta";
 
 export default function About() {
   return (
     <>
       <HeadMeta
         title="About Me"
-        description="kca2250の自己紹介です"
+        description="biiigrrrの中の人の自己紹介です"
         type="website"
         url=""
         imageUrl=""
       />
       <p>this is about page.</p>
     </>
-  )
+  );
 }
-
