f0ae27c8d13a03fee84e5ca02bf85601c8c4a122
Kenichiro Kato <72685656+kca2250@users.noreply.github.com>
2024-01-26 23:05:47 +0900

    Fix/modify title tag (#15)
    * ブログタイトル変更に伴う修正など
    
    * 軽微な修正
    
    * formatter

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,10 @@
 import dayjs from "dayjs";
 import Link from "next/link";
 
-export const Footer = () => (
-  <footer className='text-center'>
-    <p className="text-gray-500">
-      @{dayjs().get('year')} kca2250 log
-    </p>
-    <Link href='/about'>
-      <p className='text-cyan-700'>
-        🌠about me🌠
-      </p>
-    </Link>
-  </footer>
-)
+export function Footer() {
+  return (
+    <footer className="text-center">
+      <p className="text-gray-500">@{dayjs().get("year")} biiigrrr</p>
+    </footer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,24 @@
+import dayjs from "dayjs";
 import Link from "next/link";
 import { client } from "../libs/client";
 import { Blog } from "../types/blog";
-import dayjs from "dayjs";
-import HeadMeta from "@/components/headMeta";
+import { HeadMeta } from "@/components/headMeta";
 
 export default function Home({ blog }: { blog: Blog[] }) {
-
   if (blog.length === 0) {
     return (
       <div>
         <p className="text-center mt-20">記事がありません</p>
       </div>
-    )
+    );
   }
 
   return (
     <>
       <HeadMeta
-        title="記事一覧"
-        description='某フロントエンドエンジニアの備忘録'
-        type='website'
+        title="Articles"
+        description="某フロントエンドエンジニアの備忘録"
+        type="website"
         url=""
         imageUrl=""
       />
@@ -28,16 +27,20 @@ export default function Home({ blog }: { blog: Blog[] }) {
           {blog.map((blog) => {
             const publishedAt = dayjs(blog.publishedAt).format("YYYY/MM/DD");
             return (
-
-              <li key={blog.id} className="py-4 px-2 rounded hover:bg-slate-100 hover:transition-all hover:duration-300">
-                <Link href={`/blog/${blog.id}`} className="text-lg text-slate-800 flex flex-wrap">
+              <li
+                key={blog.id}
+                className="py-4 px-2 rounded hover:bg-slate-100 hover:transition-all hover:duration-300"
+              >
+                <Link
+                  href={`/blog/${blog.id}`}
+                  className="text-lg text-slate-800 flex flex-wrap"
+                >
                   <p className="underline mr-5">{publishedAt}</p>
                   <p>{blog.title}</p>
                 </Link>
               </li>
-            )
-          }
-          )}
+            );
+          })}
         </ul>
       </div>
     </>
