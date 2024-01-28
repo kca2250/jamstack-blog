93aef36a36a061b70ee967c88703120211091a64
kca2250 <2250katok@gmail.com>
2024-01-29 01:30:18 +0900

    aboutページを作成

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,7 +10,47 @@ export default function About() {
         url=""
         imageUrl=""
       />
-      <p>this is about page.</p>
+      <article>
+        <h1>About Me | kca2250について</h1>
+        <h2>このブログについて</h2>
+        このブログのメインコンテンツはフロントエンドについてですが、なかの人の趣味についても書かれています。
+        <h2>簡単な自己紹介</h2>
+        生まれと育ちは大阪で、京都在住。
+        ReactやNext.jsを用いて業務をこなしています。
+        得意分野はコーディングはもちろん、最新情報や技術的に興味のある事柄へのアンテナが敏感であるというところです。
+        <h3>技術スタック</h3>
+        業務で利用したことのあるもののみ抜粋しています。
+        <h4>Programming Language.</h4>
+        <ul>
+          <li>Javascript</li>
+          <li>Typescript</li>
+          <li>Dart</li>
+        </ul>
+        <h4>Framework</h4>
+        <ul>
+          <li>React</li>
+          <li>Next.js（App Routerは触っていません）</li>
+          <li>React Native（Bare, Expoともに）</li>
+          <li>Flutter</li>
+        </ul>
+        <h2>簡単な経歴</h2>
+        <table className="table-fixed">
+          <tbody>
+            <tr>
+              <td>2017 ~ 2021.3</td>
+              <td>行政系の事務所へ新卒で就職</td>
+            </tr>
+            <tr>
+              <td>2021.4 ~ 2021.12</td>
+              <td>休職、退職（精神的に疲弊したため）</td>
+            </tr>
+            <tr>
+              <td>2022.1 ~ 現在</td>
+              <td>某金融系企業フロントエンドエンジニア</td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
     </>
   );
 }
