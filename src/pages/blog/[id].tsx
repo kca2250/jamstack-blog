111a5facda39ab411e1d2278c0cc7f41b2e83937
kca2250 <2250katok@gmail.com>
2024-01-29 01:30:10 +0900

    軽微な修正

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,9 +1,9 @@
 import { client } from "../../libs/client";
 import { Blog } from "../../types/blog";
 import dayjs from "dayjs";
-import { load } from 'cheerio';  // cheerioの直接参照は非推奨だったため、loadをimport
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css';
+import { load } from "cheerio"; // cheerioの直接参照は非推奨だったため、loadをimport
+import hljs from "highlight.js";
+import "highlight.js/styles/atom-one-dark.css";
 import { HeadMeta } from "@/components/headMeta";
 
 export default function BlogId({ blog }: { blog: Blog }) {
@@ -12,29 +12,35 @@ export default function BlogId({ blog }: { blog: Blog }) {
   return (
     <>
       <HeadMeta
-        type='article'
+        type="article"
         title={blog.title}
-        description={blog.description ?? ''}
+        description={blog.description ?? ""}
         url={`${process.env.NEXT_PUBLIC_URL}/blog/${blog.id}`}
         imageUrl={""}
       />
       <main className="flex flex-col gap-4">
-        <div>
-          <h1>{blog.title}</h1>
-          <div className="flex items-center justify-between my-3">
-
-            <p className='text-gray-600 text-sm'>published:{publishedAt} / updated:{updatedAt}</p>
-            <p className='px-3 rounded-sm text-sm bg-slate-500 text-white inline-block'>{blog.category.name}</p>
-          </div>
-        </div>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `${blog.content}`,
-          }}
-        />
+        <article>
+          <header>
+            <h1>{blog.title}</h1>
+            <div className="flex items-center justify-between my-3">
+              <p className="text-gray-600 text-sm">
+                published:{publishedAt} / updated:{updatedAt}
+              </p>
+              <p className="px-3 rounded-sm text-sm bg-slate-500 text-white inline-block">
+                {blog.category.name}
+              </p>
+            </div>
+          </header>
+          <section>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: `${blog.content}`,
+              }}
+            />
+          </section>
+        </article>
       </main>
     </>
-
   );
 }
 
@@ -48,11 +54,11 @@ export const getStaticProps = async (context: { params: { id: string } }) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
 
-  const $ = load(data.content);  // data.contentはmicroCMSから返されるリッチエディタ部
-  $('pre code').each((_, elm) => {
+  const $ = load(data.content); // data.contentはmicroCMSから返されるリッチエディタ部
+  $("pre code").each((_, elm) => {
     const result = hljs.highlightAuto($(elm).text());
     $(elm).html(result.value);
-    $(elm).addClass('hljs');
+    $(elm).addClass("hljs");
   });
   data.content = $.html();
 
